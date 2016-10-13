9ae87b1506d4b75ae12d90068f1d5a40e4eb24a5
CKS <ckseng86@gmail.com>
2016-10-13 11:46:06 +1100

    code cleanup and add comments

--- tictactoe.js
+++ tictactoe.js
@@ -9,56 +9,60 @@ function createBoard(gridNumber) {
   return board;
 }
 
+// Create 1D array board with 9 elements
 var gameBoard = createBoard(9);
 
-// Function to console log 1D board as 2D
-function printBoard(board) {
-  var boardDisplay = '';
-  for (var i = 0; i < board.length; i++) {
-    if (i % 3 === 0 && i !== 0) {
-      boardDisplay += '\n';
-      boardDisplay += board[i];
-    }
-    else {
-      boardDisplay += board[i];
-    }
-  }
-  console.log(boardDisplay);
-}
+//  -----
+// |0|1|2|
+// |3|4|5|  Indexing for game
+// |6|7|8|
+//  -----
+
+// Variables for players' key
+var playerOne = 'X';
+var playerTwo = 'O';
+
+// Variables for storing players' win tally
+var playerOneWin = 0;
+var playerTwoWin = 0;
+
+// Variables for storing starting player and current player
+// Initialised to player one
+var startingPlayer = playerOne;
+var currentPlayer = playerOne;
+
+// Variable for storing game state
+var gameEnds = false;
 
 // Function to change board element of given index
-function populate(board, key, index) {
-  board[index] = key;
+function populate(board, player, index) {
+  board[index] = player;
 }
 
-// Sample board to test out initial code
-var sampleBoard = [
-  'o','x','x',
-  'x','o','x',
-  'x','o','o'
-];
-
-function match(board, key, index) {
-  return key === board[index];
+// Function to check if player occupies the given index location
+function match(board, player, index) {
+  return player === board[index];
 }
 
-// Winning condition
-function checkWin(board, key) {
-  var rowWin = (match(board,key,0) && match(board,key,1) && match(board,key,2)) ||
-  (match(board,key,3) && match(board,key,4) && match(board,key,5)) ||
-  (match(board,key,6) && match(board,key,7) && match(board,key,8));
+// Function to check winning condition
+// true is returned if any winning combination exists
+function checkWin(board, player) {
+  var rowWin = (match(board,player,0) && match(board,player,1) && match(board,player,2)) ||
+  (match(board,player,3) && match(board,player,4) && match(board,player,5)) ||
+  (match(board,player,6) && match(board,player,7) && match(board,player,8));
 
-  var columnWIn = (match(board,key,0) && match(board,key,3) && match(board,key,6)) ||
-  (match(board,key,1) && match(board,key,4) && match(board,key,7)) ||
-  (match(board,key,2) && match(board,key,5) && match(board,key,8));
+  var columnWIn = (match(board,player,0) && match(board,player,3) && match(board,player,6)) ||
+  (match(board,player,1) && match(board,player,4) && match(board,player,7)) ||
+  (match(board,player,2) && match(board,player,5) && match(board,player,8));
 
-  var diagWin = (match(board,key,0) && match(board,key,4) && match(board,key,8)) ||
-  (match(board,key,6) && match(board,key,4) && match(board,key,2));
+  var diagWin = (match(board,player,0) && match(board,player,4) && match(board,player,8)) ||
+  (match(board,player,6) && match(board,player,4) && match(board,player,2));
 
   return rowWin || columnWIn || diagWin;
 }
 
-// Draw condition
+// Function to check draw condition
+// true is returned if board is completely filled with character
 function checkDraw(board) {
   for (var i = 0; i < board.length; i++) {
     if (board[i] === '') {
@@ -68,89 +72,95 @@ function checkDraw(board) {
   return true;
 }
 
-// Function to play the game in the console
-function play(board, key, index) {
-  populate(key, index);
-  if (checkWin(key)) {
-    console.log(key + ' Wins!');
-  }
-}
-
-// Player switch function
+// Function to switch player's turn
 function switchPlayer(current) {
-  if (current === 'O') {
-    currentPlayer = 'X';
-  } else if (current === 'X') {
-    currentPlayer = 'O';
+  if (current === playerOne) {
+    currentPlayer = playerTwo;
+  } else if (current === playerTwo) {
+    currentPlayer = playerOne;
+  }
+  return currentPlayer;
+}
+
+// Function to increment win tally for player
+function tallyWin(player) {
+  if (player === playerOne) {
+    playerOneWin += 1;
+  }
+  else if (player === playerTwo) {
+    playerTwoWin += 1;
   }
 }
 
-function resetGame(board) {
+// Function to reset game's data
+function resetGameData(board) {
   for (var i = 0; i < board.length; i++) {
     board[i] = '';
   }
   gameEnds = false;
-  currentPlayer = 'X';
+  startingPlayer = switchPlayer(startingPlayer);
+  currentPlayer = startingPlayer;
+}
+
+//----------------------------------------
+//DOM update functions
+
+function updateTallyDisplay(player) {
+  if (player === playerOne) {
+    $('#x-tally').text(playerOneWin);
+  } else if (player === playerTwo) {
+    $('#o-tally').text(playerTwoWin);
+  }
+}
+
+function updateGridDisplay(selectedGrid,player) {
+  $(selectedGrid).text(player);
+  $(selectedGrid).css({fontSize: "0vw"});
+  $(selectedGrid).animate({fontSize: "6vw"});
+}
+
+function resetGameDisplay() {
   $('.grid').text('');
   $('#result').text('');
   $('#result').css({"display":"none"});
   $('#turn').text(currentPlayer + ' turn');
 }
 
-var xWin = 0;
-var oWin = 0;
-
-function tallyWin(player) {
-  if (player === 'X') {
-    xWin += 1;
-  }
-  else if (player === 'O') {
-    oWin += 1;
-  }
-}
-
-//----------------------------------------
-//JQuery
-
-var currentPlayer = 'X';
-var gameEnds = false;
-
-function updateTally(player) {
-  if (player === 'X') {
-    $('#x-tally').text(xWin);
-  } else if (player === 'O') {
-    $('#o-tally').text(oWin);
-  }
-}
+//---------------------------------------------
+// Main game function
 
 $('.grid').on('click',function(event) {
   if (!gameEnds) {
     var index = $(event.target).index();
     if (gameBoard[index] === ''){
-      $(event.target).text(currentPlayer);
-      $(event.target).css({fontSize: "0vw"});
-      $(event.target).animate({fontSize: "6vw"});
+      updateGridDisplay(event.target,currentPlayer);
       populate(gameBoard,currentPlayer,index);
+
       if (checkWin(gameBoard,currentPlayer)) {
         gameEnds = true;
-        console.log(currentPlayer + ' wins!');
         $('#result').text(currentPlayer + ' wins!').slideDown();
         tallyWin(currentPlayer);
-        updateTally(currentPlayer);
+        updateTallyDisplay(currentPlayer);
+
       } else if (checkDraw(gameBoard)) {
         gameEnds = true;
-        console.log('Draw');
         $('#result').text('Draw!').slideDown();
+
+      } else {
+        switchPlayer(currentPlayer);
+        $('#turn').text(currentPlayer + ' turn');
       }
-      switchPlayer(currentPlayer);
-      $('#turn').text(currentPlayer + ' turn');
     }
-  }
-  if (gameEnds) {
-    $('#turn').text('');
+
+    if (gameEnds) {
+      $('#turn').text('');
+    }
+    
   }
 });
 
+// Reset game when reset button is clicked
 $('#reset-btn').on('click',function(){
-  resetGame(gameBoard);
+  resetGameData(gameBoard);
+  resetGameDisplay();
 });
